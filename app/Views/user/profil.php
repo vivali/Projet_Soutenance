acb5ef2f8aee53062b7a0cb598d0e6bd1bb07639
renato duriez <renato.duriez@gmail.com>
2017-04-24 13:59:12 +0200

    show infos user and css

--- app/Views/user/profil.php
+++ app/Views/user/profil.php
@@ -2,6 +2,12 @@
 
 <?php $this->start('main_content') ?>
 
-<p>Hello <?=$w_user['username']?></p>
+<h1>Hello <?=$w_user['username']?></h1>
+<h2>Vos informations :</h2>
+</br>
+<p>Votre pseudo :<?=$w_user['username']?></p>
+<p>Votre adresse Email :<?=$w_user['email']?></p>
+<p>Date de création de votre compte :<?=$w_user['date_create']?></p>
+<p>Date de derniere connexion :<?=$w_user['date_last_connexion']?></p>
 
 <?php $this->stop('main_content') ?>
