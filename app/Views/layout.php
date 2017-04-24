1d1573ca51147a09e0ea8756c4d0451937d9b4f4
renato duriez <renato.duriez@gmail.com>
2017-04-24 11:17:45 +0200

    delete footer for now and design login page

--- app/Views/layout.php
+++ app/Views/layout.php
@@ -53,12 +53,6 @@
 
 			<?php } ?>
 		</section>
-
-		<footer>
-			<ul>
-				<li>©copyright : Matmaan ,Zboby ,Vivali ,Sakyo</li>
-			</ul>
-		</footer>
 	</div>
 	<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
--- app/Views/user/login.php
+++ app/Views/user/login.php
@@ -1,17 +1,23 @@
 <?php $this->layout('layout', ['title' => 'Connexion']) ?>
 
 <?php $this->start('main_content') ?>
-    <form action="" method="POST">
-        <div class="form-group">
-            <label>Votre pseudo ou votre email :</label>
-            <input id="username" name="username" class="form-control" type="text">
+<div class="container">
+    <div class="row">
+        <div class="col-md-6 col-md-offset-6">
+        <form action="" method="POST">
+            <div class="form-group">
+                <label>Votre pseudo ou votre email :</label>
+                <input id="username" name="username" class="form-control" type="text">
+            </div>
+            <div class="form-group">
+                <label>Votre mot de passe :</label>
+                <input id="password" name="password" class="form-control" type="password">
+            </div>
+            <button class="btn btn-info">Se connecter</button>
+        </form>
+        <strong><a href="#" title="forgetpassword">Mot de passe oublié ?</a></strong></p>
         </div>
-        <div class="form-group">
-            <label>Votre mot de passe :</label>
-            <input id="password" name="password" class="form-control" type="password">
-        </div>
-        <button class="btn btn-info">Se connecter</button>
-    </form>
-    <strong><a href="#" title="forgetpassword">Mot de passe oublié ?</a></strong></p>
+    </div>
+</div>
 
 <?php $this->stop('main_content') ?>
