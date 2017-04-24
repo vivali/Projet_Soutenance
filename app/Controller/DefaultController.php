fa439f210ef330ccded9957c0c72a573d943e20f
ryzommaster@hotmail.fr <ryzommaster@hotmail.fr>
2017-04-24 14:21:37 +0200

    function refresh sur chaque page

--- app/Controller/DefaultController.php
+++ app/Controller/DefaultController.php
@@ -3,26 +3,40 @@
 namespace Controller;
 
 use \W\Controller\Controller;
+use \Model\UserModel;
+use \Model\DefaultModel;
+use \Model\BuildingsModel;
+use \Model\RessourcesModel;
+
+$DefaultModel = new DefaultModel();
+$UserModel = new UserModel();
+$BuildingsModel = new BuildingsModel();
+$RessourcesModel = new RessourcesModel();
 
 class DefaultController extends Controller
 {
 	public function camp()
-	{
+	{	
+		$DefaultModel = new DefaultModel();
+		$DefaultModel->refreshTimer();
 		$this->show('default/camp');
 	}
 
 	public function building($idBuilding)
-	{
-
+	{	
+		$DefaultModel = new DefaultModel();
 		$ferme = new \Buildings\Ferme();
 		$ferme->SetProd();
 		var_dump($ferme->GetProd());
 		var_dump($idBuilding);
+		$DefaultModel->refreshTimer();
 		$this->show('default/building');
 	}
 
 	public function classement()
-	{
+	{	
+		$DefaultModel = new DefaultModel();
+		$DefaultModel->refreshTimer();
 		$this->show('default/classement');
 	}
 
