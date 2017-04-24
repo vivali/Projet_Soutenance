193b968301f651f05d7b42be0b77427f3c70c961
PhilippeGruet <philippe.gruet@outlook.fr>
2017-04-24 14:29:07 +0200

    Update profil

--- app/Views/user/profil.php
+++ app/Views/user/profil.php
@@ -3,5 +3,6 @@
 <?php $this->start('main_content') ?>
 
 <p>Hello <?=$w_user['username']?></p>
+<a href="<?=$this->url('user_update')?>">Modifier mes informations</a>
 
 <?php $this->stop('main_content') ?>
